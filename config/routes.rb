2e01c9d266ed036c7dafe5ab95086565b53a9964
ryo-ima <i.riino5y@gmail.com>
2019-09-08 19:36:50 +0900

    add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  root to: 'massages#index'  #ルートパスをmassagesに仮置き
-  resources :massages, only:[:index]
-=======
   devise_for :users
   root to: 'groups#index' 
   resources :users, only:[:edit,:update]
   resources :groups, only:[:new, :create, :edit, :update] do
     resources :messages, only:[:index,:create]
   end
->>>>>>> Stashed changes
 end
