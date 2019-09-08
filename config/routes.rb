153aad7586f8b051805736c864890986d33dea2a
ryo-ima <i.riino5y@gmail.com>
2019-09-08 19:28:55 +0900

    copy_messages_index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   root to: 'massages#index'  #ルートパスをmassagesに仮置き
   resources :massages, only:[:index]
+=======
+  devise_for :users
+  root to: 'groups#index' 
+  resources :users, only:[:edit,:update]
+  resources :groups, only:[:new, :create, :edit, :update] do
+    resources :messages, only:[:index,:create]
+  end
+>>>>>>> Stashed changes
 end
