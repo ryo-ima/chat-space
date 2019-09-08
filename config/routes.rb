e1d60a04103befdd21553602265c8e190a528165
ryo-ima <i.riino5y@gmail.com>
2019-09-08 18:40:40 +0900

    create_groups_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'messages#index'  #ルートパスをmassagesに仮置き
+  root to: 'groups#index'  #ルートパスをmassagesに仮置き
   resources :users, only:[:edit,:update]
   resources :groups, only:[:new, :create, :edit, :update] do
     resources :messages, only:[:index,:create]
