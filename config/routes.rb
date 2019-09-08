d2b4b2f1982b17cfc6178c223844424e28950d75
ryo-ima <i.riino5y@gmail.com>
2019-09-08 17:57:31 +0900

    create_user_view_and_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'massages#index'  #ルートパスをmassagesに仮置き
-  resources :massages, only:[:index]
+  devise_for :users
+  root to: 'messages#index'  #ルートパスをmassagesに仮置き
+  resources :users, only:[:edit,:update]
+  resources :groups, only:[:new, :create, :edit, :update] do
+    resources :messages, only:[:index,:create]
+  end
 end
