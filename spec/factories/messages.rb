e1e8515318c469c1e8bcf4daa17dd16ab491d8b1
ryo-ima <i.riino5y@gmail.com>
2019-09-11 12:19:35 +0900

    install_rspec_and_factorybot

---
+++ spec/factories/messages.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :message do
+    content               {"testest"}
+    image                 {"ttest.jpeg"}
+    user_id               {"1"}
+    group_id              {""}
+  end
+
+end
