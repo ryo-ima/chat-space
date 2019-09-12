23d86cfe414032476930ceda0d01bb2808e7d951
ryo-ima <i.riino5y@gmail.com>
2019-09-12 17:44:14 +0900

    Add_message_and_scroll_and_error

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,53 @@
 $(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="contents-main-right__user">
+                  <div class="contents-main-right__user-name">
+                        ${message.user_name}
+                    </div>
+                  <div class="contents-main-right__user-timestamp">
+                        ${message.date}
+                    </div>
+                </div>
+                  <div class="contents-main-right__message">
+                      ${content}
+                      ${img}
+                    </div>`
+  return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
+    var url = (window.location.href);
+    $.ajax({  
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.contents-main-right').append(html);
+      $('#message_content').val('');
+      $('#message_image').val('');
+      scrollBottom();
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.contents-main-message-form__send').prop('disabled', false)
+    })
   })
 });
+
+function scrollBottom(){
+  var target = $('.contents-main-right__user').last();
+  var position = target.offset().top + $('.contents-main-right').scrollTop();
+  $('.contents-main-right').animate({
+    scrollTop: position
+  }, 300, 'swing');
+}
