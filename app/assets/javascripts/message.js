8e96db6f4d440b848f149396c76c65f8c39fd86a
ryo-ima <i.riino5y@gmail.com>
2019-09-13 20:24:50 +0900

    add_custom_data

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,17 +2,19 @@ $(document).on('turbolinks:load', function(){
   function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="contents-main-right__user">
-                  <div class="contents-main-right__user-name">
+    var html = `<div data-messageid="${message.id}">
+                <div class="contents-main-right-message__user">
+                  <div class="contents-main-right-message__user-name">
                         ${message.user_name}
                     </div>
-                  <div class="contents-main-right__user-timestamp">
+                  <div class="contents-main-right-message__user-timestamp">
                         ${message.date}
                     </div>
                 </div>
-                  <div class="contents-main-right__message">
+                  <div class="contents-main-right-message__message">
                       ${content}
                       ${img}
+                    </div>
                     </div>`
   return html;
   }
@@ -44,7 +46,7 @@ $(document).on('turbolinks:load', function(){
 });
 
 function scrollBottom(){
-  var target = $('.contents-main-right__user').last();
+  var target = $('.contents-main-right-message__user').last();
   var position = target.offset().top + $('.contents-main-right').scrollTop();
   $('.contents-main-right').animate({
     scrollTop: position
