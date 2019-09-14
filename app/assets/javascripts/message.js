be6c793a2b4f162e54f672a5372c80f3d182d2fc
ryo-ima <i.riino5y@gmail.com>
2019-09-14 10:40:09 +0900

    create_timer_and_auto_scroll

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function(){
   function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div data-messageid="${message.id}">
+    var html = `<div class="contents-main-right-message" data-messageid="${message.id}">
                 <div class="contents-main-right-message__user">
                   <div class="contents-main-right-message__user-name">
                         ${message.user_name}
@@ -18,6 +18,7 @@ $(document).on('turbolinks:load', function(){
                     </div>`
   return html;
   }
+  
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
@@ -43,38 +44,45 @@ $(document).on('turbolinks:load', function(){
       $('.contents-main-message-form__send').prop('disabled', false)
     })
   })
+
+  function scrollBottom(){
+    var target = $('.contents-main-right-message__user').last();
+    var position = target.offset().top + $('.contents-main-right').scrollTop();
+    $('.contents-main-right').animate({
+      scrollTop: position
+    }, 300, 'swing');
+  }
+
+  var reloadMessages = function() {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){   
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  var last_message_id = $('.contents-main-right-message:last').data('messageid');
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: 'api/messages#index' ,
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      messages.forEach(function(message){
+      //メッセージが入ったHTMLを取得
+      insertHTML = buildHTML(message);
+      //メッセージを追加
+      $('.contents-main-right').append(insertHTML);
+      //スクロール
+      $('.contents-main-right').animate({scrollTop: $('.contents-main-right')[0].scrollHeight}, 'fast');
+    });
+  })
+    .fail(function() {
+      console.log('error');
+      });
+    };
+  };
+  setInterval(reloadMessages, 5000);
 });
-
-function scrollBottom(){
-  var target = $('.contents-main-right-message__user').last();
-  var position = target.offset().top + $('.contents-main-right').scrollTop();
-  $('.contents-main-right').animate({
-    scrollTop: position
-  }, 300, 'swing');
-}
-
-// var reloadMessages = function () {
-//   if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
-//     var last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
-//     // var group_id = $(".group").data("group-id");
-
-//     $.ajax({ //ajax通信で以下のことを行う
-//       url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
-//       type: 'get', //メソッドを指定
-//       dataType: 'json', //データはjson形式
-//       data: {last_id: last_message_id} //飛ばすデータは先ほど取得したlast_message_id。またparamsとして渡すためlast_idとする。
-//     })
-//     .done(function (message) { //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
-//       var insertHTML = '';//追加するHTMLの入れ物を作る
-//       message.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-//         insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
-//         $('.messages').append(insertHTML);//メッセージを追加
-//       })
-//       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
-//     })
-//     .fail(function () {
-//       alert('自動更新に失敗しました');//ダメだったらアラートを出す
-//     });
-//   }
-// };
-// setInterval(reloadMessages, 5000);
